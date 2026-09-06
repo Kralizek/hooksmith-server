af5c0f8e2d6da1cb46be7bac1ebe707cf96ba7fc
Renato Golia <renato@golia.tech>
2026-09-06 15:02:52 +0200

    Use explicit import host override

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -eu
 
-DEFAULT_IMPORT_HOSTS="deno.land,jsr.io,esm.sh,raw.esm.sh,cdn.jsdelivr.net,raw.githubusercontent.com,gist.githubusercontent.com"
-
 if [ -n "${HOOKSMITH_IMPORT_HOSTS:-}" ] && [ "${HOOKSMITH_ACCEPT_REMOTE_SOURCES:-false}" != "true" ]; then
   echo "HOOKSMITH_IMPORT_HOSTS requires HOOKSMITH_ACCEPT_REMOTE_SOURCES=true." >&2
   exit 1
@@ -20,7 +18,7 @@ if [ "${HOOKSMITH_ACCEPT_REMOTE_SOURCES:-false}" = "true" ]; then
       --allow-read \
       --allow-env \
       --allow-net \
-      "--allow-import=${DEFAULT_IMPORT_HOSTS},${HOOKSMITH_IMPORT_HOSTS}" \
+      "--allow-import=${HOOKSMITH_IMPORT_HOSTS}" \
       "jsr:@hooksmith/server@${HOOKSMITH_VERSION}" \
       "$@"
   fi
